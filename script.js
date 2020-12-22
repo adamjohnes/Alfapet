783cd6238f5f2cc521d44cb37ab598430d80f6fa
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2020-12-22 16:48:10 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -7,74 +7,58 @@
         this.width = width;
         this.height = height; //highest side
     }
-
     getLength(){
         return this.length;
     }
-
     getWidth(){
         return this.width;
     }
-
     getHeight(){
         return this.height;
     }
-
     getDimensions(){
         return String(this.length) + "x" + String(this.width) + "x" + String(this.height);
     }
-
     setLength(length){
         self.length = length;
     }
-
     setWidth(width){
         self.width = width;
     }
-
     setHeight(height){
         self.height = height;
     }
-
     setDimensions(length, width, height){
         self.length = length;
         self.width = width;
         self.height = height;
     }
 }
-
 class Liner{
     constructor(length, width){
         this.length = length;
         this.width = width;
     }
-
     getLength(){
         return this.length;
     }
-
     getWidth(){
         return this.width;
     }
-
     getDimensions(){
         return String(this.length) + "x" + String(this.width);
     }
-
     setLength(length){
         this.length = length;
     }
-
     setWidth(width){
         this.width = width;
     }
-
     setDimensions(length, width){
         this.length = length;
         this.width = width;
     }
 }
-
 let reqMed = Liner(31, 17 * 2);
 let XLJumbo = Liner(37, 18 * 2);
 let superJumbo = Liner(40, 22 * 2); */
@@ -92,12 +76,21 @@ function checkValues(){
     if (length < 0 || width < 0 || height < 0){
         if (length < 0 || isNaN(length)){
             document.getElementById("lengthError").innerHTML = "Enter a positive number.";
+            document.getElementById("output").innerHTML = "";
+        }else{
+            document.getElementById("lengthError").innerHTML = "";
         }
         if (width < 0){
             document.getElementById("widthError").innerHTML = "Enter a positive number.";
+            document.getElementById("output").innerHTML = "";
+        }else{
+            document.getElementById("widthError").innerHTML = "";
         }
         if (height < 0){
             document.getElementById("heightError").innerHTML = "Enter a positive number.";
+            document.getElementById("output").innerHTML = "";
+        }else{
+            document.getElementById("heightError").innerHTML = ""; 
         }
     }else{
         document.getElementById("lengthError").innerHTML = "";
