8f4de80298993c125846b7f38ffa5208c0378a16
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2021-01-18 13:36:09 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,6 @@
 //Javascript file for Alfapet Liner Calculator
 //Developed by Adam Johnes
 
-/* class LitterBox{
-    constructor(length, width, height){
-        this.length = length;
-        this.width = width;
-        this.height = height; //highest side
-    }
-    getLength(){
-        return this.length;
-    }
-    getWidth(){
-        return this.width;
-    }
-    getHeight(){
-        return this.height;
-    }
-    getDimensions(){
-        return String(this.length) + "x" + String(this.width) + "x" + String(this.height);
-    }
-    setLength(length){
-        self.length = length;
-    }
-    setWidth(width){
-        self.width = width;
-    }
-    setHeight(height){
-        self.height = height;
-    }
-    setDimensions(length, width, height){
-        self.length = length;
-        self.width = width;
-        self.height = height;
-    }
-}
-class Liner{
-    constructor(length, width){
-        this.length = length;
-        this.width = width;
-    }
-    getLength(){
-        return this.length;
-    }
-    getWidth(){
-        return this.width;
-    }
-    getDimensions(){
-        return String(this.length) + "x" + String(this.width);
-    }
-    setLength(length){
-        this.length = length;
-    }
-    setWidth(width){
-        this.width = width;
-    }
-    setDimensions(length, width){
-        this.length = length;
-        this.width = width;
-    }
-}
-let reqMed = Liner(31, 17 * 2);
-let XLJumbo = Liner(37, 18 * 2);
-let superJumbo = Liner(40, 22 * 2); */
-
 document.getElementById("myBtn").onclick = checkValues;
 document.getElementById("reset").onclick = resetValues;
 
@@ -83,12 +21,13 @@ function checkValues(){
     output = document.getElementById("output");
     buffer = 1;
 
-    if (length < 0 || isNaN(length) || width < 0 || isNaN(width) || lengthFront < 0 || isNaN(lengthFront) || widthFront < 0 || isNaN(widthFront) || lengthBack < 0 || isNaN(lengthBack) || widthBack < 0 || isNaN(widthBack)){
+    if (length < 0 || length == 0 || isNaN(length) || width < 0 || width == 0 || isNaN(width) || lengthFront < 0 || lengthFront == 0 || isNaN(lengthFront) || widthFront < 0 || widthFront == 0 || isNaN(widthFront) || lengthBack < 0 || lengthBack == 0 || isNaN(lengthBack) || widthBack < 0 || widthBack == 0 || isNaN(widthBack)){
         if (length < 0 || isNaN(length)){
             lengthError.innerHTML = "Enter a positive number.";
             output.innerHTML = "";
         } else if (length == 0){
             lengthError.innerHTML = "Must be larger than zero.";
+            output.innerHTML = "";
         } else{
             lengthError.innerHTML = "";
         } if (width < 0 || isNaN(width)){
@@ -96,6 +35,7 @@ function checkValues(){
             output.innerHTML = "";
         } else if (width == 0){
             widthError.innerHTML = "Must be larger than zero.";
+            output.innerHTML = "";
         } else{
             widthError.innerHTML = "";
         } if (lengthFront < 0 || isNaN(lengthFront)){
@@ -103,6 +43,7 @@ function checkValues(){
             output.innerHTML = "";
         } else if (lengthFront == 0){
             lengthFrontError.innerHTML = "Must be larger than zero.";
+            output.innerHTML = "";
         } else{
             lengthFrontError.innerHTML = "";
         } if (widthFront < 0 || isNaN(widthFront)){
@@ -110,6 +51,7 @@ function checkValues(){
             output.innerHTML = "";
         } else if (widthFront == 0){
             widthFrontError.innerHTML = "Must be larger than zero.";
+            output.innerHTML = "";
         } else{
             widthFrontError.innerHTML = "";
         } if (lengthBack < 0 || isNaN(lengthBack)){
@@ -117,6 +59,7 @@ function checkValues(){
             output.innerHTML = "";
         } else if (lengthBack == 0){
             lengthBackError.innerHTML = "Must be larger than zero.";
+            output.innerHTML = "";
         } else{
             lengthBackError.innerHTML = "";
         } if (widthBack < 0 || isNaN(widthBack)){
@@ -124,23 +67,43 @@ function checkValues(){
             output.innerHTML = "";
         } else if (widthBack == 0){
             widthBackError.innerHTML = "Must be larger than zero.";
+            output.innerHTML = "";
         } else{
             widthBackError.innerHTML = "";
         }
     }else{
+        document.getElementById("lengthError").innerHTML = "";
+        document.getElementById("widthError").innerHTML = "";
+        document.getElementById("lengthFrontError").innerHTML = "";
+        document.getElementById("widthFrontError").innerHTML = "";
+        document.getElementById("lengthBackError").innerHTML = "";
+        document.getElementById("widthBackError").innerHTML = "";
         x = length + lengthFront + lengthBack;
         y = width + widthFront + widthBack;
         if ((x <= 31 && y <= 34) && (length <= 20 && width <= 20 && lengthFront <= 8 && widthFront <= 8 && lengthBack <= 8 && widthBack <= 8)){
-            output.innerHTML = "191";
+            output.innerHTML = "The correct size is Large.<br> Click on the picture below to be taken to our Amazon store page!";
+            output.style.color = "purple";
             if (length == 0 || width == 0 || lengthFront == 0 || widthFront == 0 || lengthBack == 0 || widthBack == 0){
                 output.innerHTML = "";
             }
         } else if ((x <= 37 && y <= 36) && (length <= 24 && width <= 24 && lengthFront <= 10 && widthFront <= 10 && lengthBack <= 10 && widthBack <= 10)){
-            output.innerHTML = "192";
+            output.innerHTML = "The correct size is Extra Giant.<br> Click on the picture below to be taken to our Amazon store page!";
+            output.style.color = "rgb(94, 19, 31)";
         } else if ((x <= 42 && y <= 40.5) && (length <= 27 && width <= 26 && lengthFront <= 12 && widthFront <= 12 && lengthBack <= 12 && widthBack <= 12)){
-            output.innerHTML = "178";
-        } else{
-            output.innerHTML = "One of your measurements is too large for our liners."
+            output.innerHTML = "The correct size is Super Jumbo.<br> Click on the picture below to be taken to our Amazon store page!";
+            output.style.color = "darkgreen";
+        } else if (length > 27){
+            output.innerHTML = "Your length is too large for any for our liners."
+        } else if (width > 26){
+            output.innerHTML = "Your width is too large for any for our liners."
+        } else if (lengthFront > 12){
+            output.innerHTML = "Your front side is too large for any for our liners."
+        } else if (widthFront > 12){
+            output.innerHTML = "Your left side is too large for any for our liners."
+        } else if (lengthBack > 12){
+            output.innerHTML = "Your back side is too large for any for our liners."
+        } else if (widthBack > 12){
+            output.innerHTML = "Your right side is too large for any for our liners."
         }
     }
 }
@@ -152,11 +115,11 @@ function resetValues(){
     document.getElementById("widthFront").value = null;
     document.getElementById("lengthBack").value = null;
     document.getElementById("widthBack").value = null;
-    document.getElementById("lengthError").value = null;
-    document.getElementById("widthError").value = null;
-    document.getElementById("lengthFrontError").value = null;
-    document.getElementById("widthFrontError").value = null;
-    document.getElementById("lengthBackError").value = null;
-    document.getElementById("widthBackError").value = null;
+    document.getElementById("lengthError").innerHTML = "";
+    document.getElementById("widthError").innerHTML = "";
+    document.getElementById("lengthFrontError").innerHTML = "";
+    document.getElementById("widthFrontError").innerHTML = "";
+    document.getElementById("lengthBackError").innerHTML = "";
+    document.getElementById("widthBackError").innerHTML = "";
     output = document.getElementById("output").innerHTML = "";
 }
